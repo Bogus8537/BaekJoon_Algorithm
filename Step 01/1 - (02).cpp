3efead2af614a45a3720141e597f42af273fde25
Bogus8537 <57387097+Bogus8537@users.noreply.github.com>
2021-04-18 22:00:40 +0900

    Add files via upload

---
+++ Step 01/1 - (02).cpp
@@ -0,0 +1,27 @@
+/*
+	백준 Q.1 - 02 (10718)
+	대회를 뜰 줄 모르는 지박령 kriii를 위해서 격려의 문구를 출력
+*/
+/*
+
+#define MAX 20
+#include <stdio.h>
+
+int main()
+{
+	char s[MAX] = "강한친구 대한육군";
+	printf("%s\n%s\n\n", s, s);
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main()
+{
+	printf("강한친구 육군\n");
+	printf("강한친구 육군\n");
+
+	return 0;
+}
+*/
