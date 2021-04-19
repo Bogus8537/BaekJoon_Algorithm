a3c0f1c6f9bd597b7bf2349de53bbe310dcc5799
Bogus8537 <57387097+Bogus8537@users.noreply.github.com>
2021-04-19 18:52:27 +0900

    Step 01. 1 - (09).c

--- Step 01/1 - (09).c
+++ Step 01/1 - (09).c
@@ -1,9 +1,11 @@
+// This code was written as C.
+// There're inputting two numbers in command and calculate " +, -, *, /, % " 
+
 #include <stdio.h>
 
 int main()
 {
 	int a, b;
-
 	scanf("%d %d", &a, &b);
 
 	printf("%d\n", a + b);
@@ -11,5 +13,6 @@ int main()
 	printf("%d\n", a * b);
 	printf("%d\n", a / b);
 	printf("%d\n", a % b);
+	
 	return 0;
 }
