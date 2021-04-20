eae44b172aae338e1d9b0da1fd1f2e540e8c5834
Bogus8537 <57387097+Bogus8537@users.noreply.github.com>
2021-04-20 13:59:17 +0900

    Step 02 - (04).c

--- Step 02/2 - (04).c
+++ Step 02/2 - (04).c
@@ -10,29 +10,28 @@
 
 int main()
 {
-  int x, y;
+	int x, y;
+	
+	scanf("%d %d", &x, &y);
   
-  scanf("%d %d", &x, &y);
-  
-  if (x > 0 && y > 0)
-		{
-		  printf("1\n");
-		}
-
-		else if (x < 0 && y > 0)
-		{
-			printf("2\n");
-		}
-
-		else if (x < 0 && y < 0)
-		{
-			printf("3\n");
-		}
-
-		else if (x > 0 && y < 0)
-		{
-			printf("4\n");
-		}
-  
-  return 0;
+	if (x > 0 && y > 0)
+	{	 
+		printf("1\n");
+	}
+	
+	else if (x < 0 && y > 0)
+	{
+		printf("2\n");
+	}
+	
+	else if (x < 0 && y < 0)
+	{
+		printf("3\n");	
+	}	
+	
+	else if (x > 0 && y < 0)
+	{
+		printf("4\n");	
+	}
+  	return 0;
 }
