db79489cd89798075af6db320e1661423e73f0f9
Bogus8537 <57387097+Bogus8537@users.noreply.github.com>
2021-04-22 13:43:05 +0900

    Step 04 - (02).c

---
+++ Step 04/4 - (02).c
@@ -0,0 +1,20 @@
+// BaekJoon Q4 - 02 (10951)
+// This program's similar with Step 04 - (01).c
+// There's a part of addtion comparing 4 - (01).c
+// It added EOF condition to exit this program when users want to end.
+
+#include <stdio.h>
+
+int main()
+{
+	int A, B, sum;
+		
+	while (scanf("%d %d", &A, &B) != EOF)   // Here's the point in this code! Press Ctrl+Z to exit this program.
+	{
+		sum = A + B;
+		printf("%d\n", sum);
+	}
+
+	printf("\n");
+	return 0;
+}
